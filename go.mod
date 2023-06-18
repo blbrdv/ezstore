84a4b56f1b17ed070fc4434cb8a0ed4f1ceef654
blbrdv <95272917+blbrdv@users.noreply.github.com>
2023-06-19 00:09:43 +0700

    add tests

--- go.mod
+++ go.mod
@@ -5,7 +5,12 @@ go 1.19
 require github.com/urfave/cli/v2 v2.25.6
 
 require (
+	github.com/antchfx/xmlquery v1.3.17 // indirect
+	github.com/antchfx/xpath v1.2.4 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 )
---
+++ payloads/payloads_test.go
@@ -0,0 +1,95 @@
+package payloads
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/antchfx/xmlquery"
+)
+
+func TestFE3FileUrl(t *testing.T) {
+	expectedTycketType := "aaaaa"
+	expectedId := "bbbbb"
+	expectedRevisionNumber := "ccccc"
+
+	xml := strings.NewReader(FE3FileUrl(expectedTycketType, expectedId, expectedRevisionNumber))
+
+	doc, err := xmlquery.Parse(xml)
+	if err != nil {
+		t.Fatalf(`Can not parse XML file`)
+	}
+
+	root := xmlquery.FindOne(doc, "//s:Envelope")
+
+	actualTicketType := root.
+		SelectElement("//s:Header/o:Security/wuws:WindowsUpdateTicketsToken/TicketType").
+		InnerText()
+	if actualTicketType != expectedTycketType {
+		t.Fatalf(`Incorrect TycketType, expected: "%s", actual: "%s"`, expectedTycketType, actualTicketType)
+	}
+
+	identity := root.
+		SelectElement("//s:Body/GetExtendedUpdateInfo2/updateIDs/UpdateIdentity")
+
+	actualId := identity.
+		SelectElement("//UpdateID").
+		InnerText()
+	if actualId != expectedId {
+		t.Fatalf(`Incorrect TycketType, expected: "%s", actual: "%s"`, expectedId, actualId)
+	}
+
+	actualRevisionNumber := identity.
+		SelectElement("//RevisionNumber").
+		InnerText()
+	if actualRevisionNumber != expectedRevisionNumber {
+		t.Fatalf(`Incorrect TycketType, expected: "%s", actual: "%s"`, expectedRevisionNumber, actualRevisionNumber)
+	}
+}
+
+func TestGetCookie(t *testing.T) {
+	xml := strings.NewReader(GetCookie())
+
+	_, err := xmlquery.Parse(xml)
+	if err != nil {
+		t.Fatalf(`Can not parse XML file`)
+	}
+}
+
+func TestWUIDRequest(t *testing.T) {
+	expectedTycketType := "aaaaa"
+	expectedCookie := "bbbbb"
+	expectedCategoryIdentifier := "ccccc"
+
+	xml := strings.NewReader(WUIDRequest(expectedTycketType, expectedCookie, expectedCategoryIdentifier))
+
+	doc, err := xmlquery.Parse(xml)
+	if err != nil {
+		t.Fatalf(`Can not parse XML file`)
+	}
+
+	root := xmlquery.FindOne(doc, "//s:Envelope")
+
+	actualTicketType := root.
+		SelectElement("//s:Header/o:Security/wuws:WindowsUpdateTicketsToken/TicketType").
+		InnerText()
+	if actualTicketType != expectedTycketType {
+		t.Fatalf(`Incorrect TycketType, expected: "%s", actual: "%s"`, expectedTycketType, actualTicketType)
+	}
+
+	syncUpdates := root.
+		SelectElement("//s:Body/SyncUpdates")
+
+	actualCookie := syncUpdates.
+		SelectElement("//cookie/EncryptedData").
+		InnerText()
+	if actualCookie != expectedCookie {
+		t.Fatalf(`Incorrect TycketType, expected: "%s", actual: "%s"`, expectedCookie, actualCookie)
+	}
+
+	actualCategoryIdentifier := syncUpdates.
+		SelectElement("//parameters/FilterAppCategoryIds/CategoryIdentifier/Id").
+		InnerText()
+	if actualCategoryIdentifier != expectedCategoryIdentifier {
+		t.Fatalf(`Incorrect TycketType, expected: "%s", actual: "%s"`, expectedCategoryIdentifier, actualCategoryIdentifier)
+	}
+}
