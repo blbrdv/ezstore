1006658ae08df8136d7e3d33739c14bbcd101770
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-16 16:11:03 +0700

    Add PS lint

---
+++ pslint.ps1
@@ -0,0 +1,28 @@
+Set-StrictMode -Version 3.0;
+$ErrorActionPreference = "Stop";
+trap { Write-Error $_ -ErrorAction Continue; exit 1 };
+
+Install-Module -Name PSScriptAnalyzer;
+
+$Settings = @{
+    ExcludeRules = @(
+        'PSAvoidGlobalVars',
+        'PSUseDeclaredVarsMoreThanAssignments',
+        'PSAvoidUsingInvokeExpression'
+    )
+}
+
+$Files = Get-ChildItem -Path .\*.ps1 -Recurse;
+$Found = $false;
+
+foreach ($File in $Files) {
+    $Output = Invoke-ScriptAnalyzer -Path $File -Settings $Settings ;
+    foreach ($Data in $Output) {
+        $Found = $true;
+        Write-Output "$($Data.ScriptName):$($Data.Line) $($Data.Message)";
+    }
+}
+
+if ( $Found ) {
+    exit 1;
+}
