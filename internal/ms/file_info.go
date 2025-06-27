1e404109710f81e05660277e69516e69368fa1c5
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-27 14:08:50 +0700

    Add string function

--- internal/ms/file_info.go
+++ internal/ms/file_info.go
@@ -1,8 +1,14 @@
 package ms
 
+import "fmt"
+
 // FileInfo represents useful information about file bundle.
 type FileInfo struct {
 	Path    string
 	Name    string
 	Version *Version
+}
+
+func (fi *FileInfo) String() string {
+	return fmt.Sprintf("%s %s '%s'", fi.Name, fi.Version, fi.Path)
 }
