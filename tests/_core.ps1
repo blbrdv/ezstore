39ee67b8883c83e14ebe1bf6f11134c86d7fcad4
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-07-01 20:45:55 +0700

    Fix tests flow

--- tests/_core.ps1
+++ tests/_core.ps1
@@ -6,13 +6,27 @@ function Install {
         [Parameter(Mandatory=$true,Position=0)]
         [string]$Id,
         [Parameter(Mandatory=$true,Position=1)]
-        [string]$Version
+        [string]$Version,
+        [Parameter(Position=2)]
+        [int]$ExpectedCode
     )
 
+    if ( $null -eq $ExpectedCode ) {
+        $ExpectedCode = 0;
+    }
+
+    $global:LASTEXITCODE = $null;
+
     $Cmd = ".\output\bin\ezstore.exe install $Id --ver $Version --verbosity d";
-    Write-Output $Cmd;
+    Write-Host $Cmd;
     $Output = Invoke-Expression -Command "$Cmd 2>&1";
-    Write-Output $Output;
+    foreach ($Line in ($Output -split "\n")) {
+        Write-Host $Line;
+    }
+
+    if ( $ExpectedCode -ne $global:LASTEXITCODE ) {
+        throw "expected exit code $ExpectedCode, actual $($global:LASTEXITCODE)"
+    }
 
     return $Output;
 
