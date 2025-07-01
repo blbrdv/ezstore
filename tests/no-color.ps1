b26a9d29af4247e80d6fa5a20be907885c66a083
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-07-01 19:51:27 +0700

    Update scripts

--- tests/no-color.ps1
+++ tests/no-color.ps1
@@ -4,12 +4,13 @@ trap { Write-Error $_ -ErrorAction Continue; exit 1 };
 
 . ".\tests\_core.ps1";
 
+$Key = $($Targets.Keys)[2];
+
 $Env:NO_COLOR = "1"
-$Output = Install $Data.Id $Data.Version;
+$Output = Install $Key $Targets[$Key].Version;
 
 if ( $Output -match $ColorRegexp ) {
     throw 'Output has colors.';
 }
 
 Write-Output "Test passed!";
-
