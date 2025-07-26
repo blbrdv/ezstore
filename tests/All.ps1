db530bc844303ac6e6d1fc5af2f414c1128029d7
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-07-26 12:19:33 +0700

    Fix typo

--- tests/All.ps1
+++ tests/All.ps1
@@ -23,7 +23,7 @@ $Targets = $Archs -split "," | ForEach-Object {
 };
 
 Import-ModuleSafe -Name "Pester" -Version "5.7.1";
-Import-ModuleSafe-Name "Pester" -Version "5.7.1";
+Import-ModuleSafe -Name "Pester" -Version "5.7.1";
 
 # For some reason default glob search in New-PesterContainer Path parameter didn't work for me so using Get-ChildItem
 $Containers = Get-ChildItem -Path $PSScriptRoot -Filter "*.Tests.ps1" -ErrorAction 'SilentlyContinue'
