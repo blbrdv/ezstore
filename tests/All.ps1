a90edf10389fc4f2591f64940b0ce2115b6a7315
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-07-27 21:39:01 +0700

    Fix skip check

--- tests/All.ps1
+++ tests/All.ps1
@@ -24,6 +24,22 @@ $Targets = $Archs -split "," | ForEach-Object {
     }
 };
 
+$SkipInstallTests = $false;
+
+# skipping 386 and ARM architectures on Windows 11 ARM64 on Github VMs due to this issue
+# https://learn.microsoft.com/en-us/windows/release-health/status-windows-11-21h2#2819msgdesc
+if ( $null -ne $Env:GITHUB_ACTION ) {
+    $OSArch = [System.Runtime.InteropServices.RuntimeInformation,mscorlib]::OSArchitecture.ToString().ToLower();
+    $OSBuild = [Environment]::OSVersion.Version.BuildNum;
+
+    $Is32bitApp = ( $Arch -eq "386" ) -or ( $Arch -eq "arm" );
+    $IsArm64Win11 = ( $OSBuild -ge 22000 ) -and ( $OSArch -eq "arm64" )
+
+    if ( $IsArm64Win11 -and $Is32bitApp ) {
+        $SkipInstallTests = $True;
+    }
+}
+
 Import-ModuleSafe -Name "Pester" -Version "5.7.1";
 Import-ModuleSafe -Name "Pester" -Version "5.7.1";
 
