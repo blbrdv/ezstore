e29c7c50db9fc64292ec413d7e71327e7cebc968
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-08-05 22:39:42 +0700

    Fix code smell

--- internal/ms/windows/version.go
+++ internal/ms/windows/version.go
@@ -7,11 +7,11 @@ import (
 type WindowsVersion int
 
 const (
+	W8 WindowsVersion = iota + 8
+	W8d1
+	W10
+	W11
 	Unsupported WindowsVersion = 0
-	W8                         = 8
-	W8d1                       = 9
-	W10                        = 10
-	W11                        = 11
 )
 
 var Version = GetVersion()
@@ -19,19 +19,20 @@ var Version = GetVersion()
 func GetVersion() WindowsVersion {
 	v := w32.RtlGetVersion()
 
-	if v.MajorVersion == 10 {
+	switch v.MajorVersion {
+	case 10:
 		if v.MinorVersion >= 22000 {
 			return W11
 		} else {
 			return W10
 		}
-	} else if v.MajorVersion == 6 {
+	case 6:
 		if v.MinorVersion >= 9600 {
 			return W8d1
 		} else {
 			return W8
 		}
-	} else {
+	default:
 		return Unsupported
 	}
 }
